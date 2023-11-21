9417a0d68c49c0d8c95c724d25105a6e341cde5f
Semen <lbdw.smn@gmail.com>
2023-11-21 16:18:13 +0300

    create header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,42 @@
+
+import style from './header.module.scss'
+import { Link } from 'react-router-dom'   
+
+const Header = () => {
+  return (
+    <header className={style.header}>
+        {/* <Link to="/"> */}
+            <div className={style.header__logo}>
+                <img className={style.header__image} src="./assets/logo.svg" alt="logo" />
+                <span>Коробка</span>
+            </div>
+        {/* </Link>  */}
+        <nav className={style.navigation}>
+            <ul className={style.list__navigation}>
+                
+                {/* <Link to="#"> */}
+                    <li className={style.list__item}>
+                        <p className={style.list__text}>Закладки</p>
+                    </li>
+                {/* </Link> */}
+                {/* <Link to="#"> */}
+                    <li className={style.list__item}>
+                        <p className={style.list__text}>Закладки</p>
+                    </li>
+                {/* </Link> */}
+                {/* <Link to="#"> */}
+                    <li className={style.list__item}>
+                        <p className={style.list__text}>Закладки</p>
+                    </li>
+                {/* </Link> */}
+            </ul>
+        </nav>
+        <div>
+            <button className={style.button_login}>Вход</button>
+            <button className={style.button_login}>Регистрация</button>
+        </div>
+    </header>
+  );
+};
+
+export default Header;
