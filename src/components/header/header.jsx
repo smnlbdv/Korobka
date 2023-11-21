b62c6b383f482f323fa934d9ca29214e436b0789
Semen <lbdw.smn@gmail.com>
2023-11-22 01:25:37 +0300

    new style header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,29 @@
 
 import style from './header.module.scss'
-// import { Link } from 'react-router-dom'   
+import './header.css'
+import { Dropdown, Space } from 'antd';
+// import { Link } from 'react-router-dom' 
+
+
+const items = [
+    {
+        label: <a  href="#">Для мужчин</a>,
+        key: '0',
+    },
+    {
+        label: <a href="#">На 14 февраля</a>,
+        key: '1',
+    },
+    {
+        label: <a href="#">Для девушек</a>,
+        key: '2',
+    },
+    
+];
+
 
 const Header = () => {
+
   return (
     <header className={style.header}>
         {/* <Link to="/"> */}
@@ -16,22 +37,31 @@ const Header = () => {
                 
                 {/* <Link to="#"> */}
                     <li className={style.list__item}>
-                        <p className={style.list__text}>Закладки</p>
+                        <p className={style.list__text}>Собрать</p>
                     </li>
                 {/* </Link> */}
                 {/* <Link to="#"> */}
-                    <li className={style.list__item}>
-                        <p className={style.list__text}>Закладки</p>
-                    </li>
+                <Dropdown menu={{ items }} trigger={['click']}>
+                    <a onClick={(e) => e.preventDefault()}>
+                        <Space>
+                            Готовые подарки
+                        </Space>
+                    </a>
+                </Dropdown>
                 {/* </Link> */}
                 {/* <Link to="#"> */}
                     <li className={style.list__item}>
-                        <p className={style.list__text}>Закладки</p>
+                        <p className={style.list__text}>Контакты</p>
+                    </li>
+                {/* </Link> */}
+                {/* <Link to="#"> */}
+                <li className={style.list__item}>
+                        <p className={style.list__text}>О нас</p>
                     </li>
                 {/* </Link> */}
             </ul>
         </nav>
-        <div>
+        <div className={style.buttons_block}>
             <button className={style.button_login}>Вход</button>
             <button className={style.button_login}>Регистрация</button>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { StyleProvider } from '@ant-design/cssinjs';
 
 import App from './App.jsx'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <StyleProvider hashPriority="high">
       <App />
+    </StyleProvider>
   </React.StrictMode>,
 )
